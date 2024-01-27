e107ace2e4e82f5571659cdb77bc0b00ab96582d
pythonstrup <pythonstrup@gmail.com>
2024-01-27 22:55:35 +0900

    atomic operations, volatile

--- multi-thread/src/Main.java
+++ multi-thread/src/Main.java
@@ -1,10 +1,11 @@
 import java.io.IOException;
 import race.condition.SynchronizedBlock;
+import race.condition.atomic.operations.AtomicOperations;
 
 public class Main {
 
   public static void main(String[] args) throws InterruptedException, IOException {
-    SynchronizedBlock.execute();
+    AtomicOperations.execute();
   }
 
 }
