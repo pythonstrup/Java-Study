6097bd7d486cbcfdf8cffd02f15648e8f64a98e2
pythonstrup <pythonstrup@gmail.com>
2024-01-29 22:29:56 +0900

    Deadlock

--- multi-thread/src/Main.java
+++ multi-thread/src/Main.java
@@ -1,11 +1,10 @@
+import deadlock.Deadlock;
 import java.io.IOException;
-import race.condition.non.atomic.NonAtomic;
-import race.condition.non.atomic.NonAtomicVolatile;
 
 public class Main {
 
   public static void main(String[] args) throws InterruptedException, IOException {
-    NonAtomicVolatile.execute();
+    Deadlock.execute();
   }
 
 }
