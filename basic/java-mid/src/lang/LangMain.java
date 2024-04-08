a02e1c33cc406ff50fa9b460d27b4d729c6e3cd1
pythonstrup <pythonstrup@gmail.com>
2024-04-08 23:25:59 +0900

    [Java] Java Object

---
+++ basic/java-mid/src/lang/LangMain.java
@@ -0,0 +1,12 @@
+package lang;
+
+// java.lang은 생략 가능!
+//import java.lang.System
+
+public class LangMain {
+
+  public static void main(String[] args) {
+    System.out.println("hello java");
+  }
+
+}
