ca5fb1161660467db0b1dc8f36e743a410055314
pythonstrup <pythonstrup@gmail.com>
2024-04-10 21:46:21 +0900

    [Java] String Method

---
+++ basic/java-mid/src/lang/string/method/StringComparisonMain.java
@@ -0,0 +1,22 @@
+package lang.string.method;
+
+public class StringComparisonMain {
+
+  public static void main(String[] args) {
+    String str1 = "Hello, Java!";
+    String str2 = "hello, java!";
+    String str3 = "Hello, World!";
+
+    System.out.println("str1 equals str2: " + str1.equals(str2));
+    System.out.println("str1 equalsIgnoreCase str2: " + str1.equalsIgnoreCase(str2));
+
+    System.out.println("'b' compareTo 'a': " + "b".compareTo("a"));
+    System.out.println("'a' compareTo 'b': " + "a".compareTo("b"));
+    System.out.println("str1 compareTo str2: " + str1.compareTo(str2));
+    System.out.println("str1 compareToIgnoreCase str2: " + str1.compareToIgnoreCase(str2));
+
+    System.out.println("str1 startsWith 'Hello': " + str1.startsWith("Hello"));
+    System.out.println("str1 startsWith 'hello': " + str1.startsWith("hello"));
+    System.out.println("str1 endsWith 'Java!': " + str1.endsWith("Java!"));
+  }
+}
