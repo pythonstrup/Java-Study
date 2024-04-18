96fc6cdc6aa2e0ee1ddf000e64a194eb90929608
pythonstrup <pythonstrup@gmail.com>
2024-04-18 22:09:02 +0900

    [Java] 내부 클래스

--- basic/java-mid/src/nested/nested/NestedOuterMain.java
+++ basic/java-mid/src/nested/nested/NestedOuterMain.java
@@ -1,12 +1,12 @@
 package nested.nested;
 
-import nested.nested.NestedOuter.Nested;
 
 public class NestedOuterMain {
 
   public static void main(String[] args) {
     NestedOuter outer = new NestedOuter();
-    Nested nested = new Nested();
+
+    NestedOuter.Nested nested = new NestedOuter.Nested();
     nested.print();
 
     System.out.println("nested.getClass() = " + nested.getClass());
