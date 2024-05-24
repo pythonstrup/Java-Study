796ab3a47da57bab0201cd983d5967e6939cfb93
pythonstrup <pythonstrup@gmail.com>
2024-05-24 22:31:45 +0900

    [Java] Generic Method

---
+++ basic/java-mid/src/generic/ex4/AnimalMethod.java
@@ -0,0 +1,16 @@
+package generic.ex4;
+
+import generic.animal.Animal;
+
+public class AnimalMethod {
+
+  public static <T extends Animal> void checkup(T t) {
+    System.out.println("animal.getName() = " + t.getName());
+    System.out.println("animal.getSize() = " + t.getSize());
+    t.sound();
+  }
+
+  public static <T extends Animal> T getBigger(T t1, T t2) {
+    return t1.getSize() > t2.getSize()? t1: t2;
+  }
+}
