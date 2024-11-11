62d9070bc821c5febff4a79c509494508a59bf58
pythonstrup <pythonstrup@gmail.com>
2024-11-11 22:32:29 +0900

    [Design Pattern] proxy pattern - Java RMI

---
+++ design-pattern/src/main/java/com/pythonstrup/proxy/rmi/MyRemote.java
@@ -0,0 +1,8 @@
+package com.pythonstrup.proxy.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface MyRemote extends Remote {
+  String sayHello() throws RemoteException;
+}
---
+++ design-pattern/src/main/java/com/pythonstrup/proxy/rmi/MyRemoteClient.java
@@ -0,0 +1,19 @@
+package com.pythonstrup.proxy.rmi;
+
+import java.rmi.Naming;
+
+public class MyRemoteClient {
+
+  public static void main(String[] args) {
+    new MyRemoteClient().go();
+  }
+
+  public void go() {
+    try {
+      MyRemote service = (MyRemote) Naming.lookup("rmi://127.0.0.1/RemoteHello");
+      String s = service.sayHello();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+}
