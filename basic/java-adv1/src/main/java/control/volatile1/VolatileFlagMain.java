d19bc807392524e26d79d3e7d3724fd8d8907625
pythonstrup <pythonstrup@gmail.com>
2025-04-06 20:41:32 +0900

    [Java] Thread volatile

---
+++ basic/java-adv1/src/main/java/control/volatile1/VolatileFlagMain.java
@@ -0,0 +1,34 @@
+package control.volatile1;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class VolatileFlagMain {
+
+  public static void main(String[] args) {
+    MyTask task = new MyTask();
+    Thread thread = new Thread(task, "work");
+    log("runFlag=" + task.runFlag);
+    thread.start();
+
+    sleep(1000);
+    log("runFlag를 false로 변경 시도");
+    task.runFlag = false;
+    log("runFlag=" + task.runFlag);
+    log("main 종료");
+  }
+
+  static class MyTask implements Runnable {
+//    boolean runFlag = true;
+    volatile boolean runFlag = true;
+
+    @Override
+    public void run() {
+      log("작업 시작");
+      while (runFlag) {
+        // runFlag가 false로 변하면 탈출
+      }
+      log("작업 종료");
+    }
+  }
+}
